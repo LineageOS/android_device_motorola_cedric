e724697e81d5a577d06f819ea65115969ba14d15
Michael Bestas <mkbestas@lineageos.org>
2019-10-08 21:47:36 +0300

    cedric: Remove android.hidl.base@1.0 shims
    * Patch the remaining blob that depends on it.
    * It already needs libhidltransport.so so there's
      no real need for the shims anymore.
    
    Change-Id: I00f185fd4d1d8798893cf82abf9d64fcb68c3301

--- extract-files.sh
+++ extract-files.sh
@@ -25,6 +25,7 @@ export DEVICE_BRINGUP_YEAR=2018
 DEVICE_BLOB_ROOT="${LINEAGE_ROOT}/vendor/${VENDOR}/${DEVICE}/proprietary"
 
 sed -i 's|/firmware/image|/vendor/f/image|' "${DEVICE_BLOB_ROOT}/vendor/bin/hw/android.hardware.biometrics.fingerprint@2.1-fpcservice"
+patchelf --remove-needed android.hidl.base@1.0.so "${DEVICE_BLOB_ROOT}/vendor/lib/com.fingerprints.extension@1.0_vendor.so"
 sed -i "s/libgui/libwui/" "${DEVICE_BLOB_ROOT}/vendor/lib/libmmcamera_ppeiscore.so"
 patchelf --add-needed libppeiscore_shim.so "${DEVICE_BLOB_ROOT}/vendor/lib/libmmcamera_ppeiscore.so"
 sed -i "s/system input/system uhid input/" "${DEVICE_BLOB_ROOT}/vendor/etc/init/android.hardware.biometrics.fingerprint@2.1-service.rc"
