ab32390a481111acf06651b9640d3ee88d239e7c
Jarl-Penguin <jarlpenguin@outlook.com>
2020-05-13 10:33:35 +0300

    montana: Import sensor config from stock
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- device.mk
+++ device.mk
@@ -73,5 +73,4 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf \
-    $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev_ahannah.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev_ahannah.conf
+    $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
