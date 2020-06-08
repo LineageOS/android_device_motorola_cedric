295e9b6d5dc6fd103cc0c9fc4abcc3168f771ba6
Jarl-Penguin <jarlpenguin@outlook.com>
2020-06-08 17:22:39 +0300

    montana: Update check_device.sh for SAR
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I9ed4683358fe651b9c192c0ff09a5ea120175120

--- configs/check_device.sh
+++ configs/check_device.sh
@@ -2,7 +2,7 @@
 
 RADIO=`getprop ro.boot.radio`
 
-rm -rf /system/priv-app/MotoDoze
+rm -rf /system_root/system/priv-app/MotoDoze
 rm /vendor/lib/egl/libEGL_adreno_8917.so
 rm /vendor/lib/egl/libGLESv1_CM_adreno_8917.so
 rm /vendor/lib/egl/libGLESv2_adreno_8917.so
@@ -15,7 +15,7 @@ if [ "$RADIO" != "APAC" ] && [ "$RADIO" != "EMEA" ]; then
     rm /vendor/etc/permissions/android.hardware.nfc.xml
     rm /vendor/etc/permissions/android.hardware.nfc.hce.xml
     rm /vendor/etc/permissions/android.hardware.nfc.hcef.xml
-    rm -r /system/app/NfcNci
+    rm -r /system_root/system/app/NfcNci
 fi
 if ["$RADIO" == "Brazil" ] || [ "$RADIO" == "APAC" ] || [ "$RADIO" == "EMEA" ]; then
     # Remove e-compass file for APAC, Brazil and EMEA RADIOs
