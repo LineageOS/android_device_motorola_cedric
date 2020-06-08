9b94b5ca8ec7d02a167d86fdf1d8b05a31b5c0f6
Jarl-Penguin <jarlpenguin@outlook.com>
2020-06-09 02:08:29 +0300

    montana: Add missing firmware symlinks
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I25b8f5f1d67c31df604c516eb31fee9cf381aa0d

--- Android.mk
+++ Android.mk
@@ -18,4 +18,19 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_DEVICE),montana)
   subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
   $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
+
+FIRMWARE_FPCTZ_IMAGES := \
+    fpctzappfingerprint.b00 fpctzappfingerprint.b01 fpctzappfingerprint.b02 \
+    fpctzappfingerprint.b03 fpctzappfingerprint.b04 fpctzappfingerprint.b05 \
+    fpctzappfingerprint.b06 fpctzappfingerprint.mdt
+
+FIRMWARE_FPCTZ_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(notdir $(FIRMWARE_FPCTZ_IMAGES)))
+$(FIRMWARE_FPCTZ_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Fpctz Firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /vendor/firmware_mnt/image/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_FPCTZ_SYMLINKS)
+
 endif
