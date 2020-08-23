d581c519b2cfef1754adafea67ef01825f111790
Jarl-Penguin <jarlpenguin@outlook.com>
2020-08-23 07:08:05 +0300

    montana: Move some props to vendor.prop
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I62fe7b79051613850931262ff7fa25616bf5006c

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -2,6 +2,18 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.gyro.disable=0
 
+# Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dex2oat-threads=6
+
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=420
+
+# GPU
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.opengles.version=196610
+
+# Sensors
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.sensors=montana
