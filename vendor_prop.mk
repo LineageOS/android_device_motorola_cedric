9f85fffba3c28bb73b904a123ce6e500467e5b41
Jarl-Penguin <jarlpenguin@outlook.com>
2020-08-26 13:38:12 +0300

    montana: Enable HAL3
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I148f3affdeed1dc3dc9d6ce65e02c4b7bda33d32

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -1,6 +1,7 @@
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.camera.gyro.disable=0
+    persist.camera.gyro.disable=0 \
+    persist.camera.HAL3.enabled=1
 
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
