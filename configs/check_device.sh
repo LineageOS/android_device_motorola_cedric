94b7f27e35ec9ca4199fa4efaf44430602bda9e9
Sohamlad7 <sohamlad7@gmail.com>
2020-09-10 19:29:09 +0530

    cedric: Remove NFC stuff
    * cedric doesn't have NFC
    
    Change-Id: I53a952f21ca4bbbc5c1c20a3978442d9e18e9cd6

--- configs/check_device.sh
+++ configs/check_device.sh
@@ -1,17 +1,3 @@
 #!/sbin/sh
 
-RADIO=`getprop ro.boot.radio`
-
 rm -rf /system_root/system/priv-app/MotoDoze
-if [ "$RADIO" != "APAC" ] && [ "$RADIO" != "EMEA" ]; then
-    # Remove NFC-related files for RADIOs other than APAC and EMEA
-    rm /vendor/etc/permissions/com.android.nfc_extras.xml
-    rm /vendor/etc/permissions/android.hardware.nfc.xml
-    rm /vendor/etc/permissions/android.hardware.nfc.hce.xml
-    rm /vendor/etc/permissions/android.hardware.nfc.hcef.xml
-    rm -r /system_root/system/app/NfcNci
-fi
-if ["$RADIO" == "Brazil" ] || [ "$RADIO" == "APAC" ] || [ "$RADIO" == "EMEA" ]; then
-    # Remove e-compass file for APAC, Brazil and EMEA RADIOs
-    rm /vendor/etc/permissions/android.hardware.sensor.compass.xml
-fi
--- device.mk
+++ device.mk
@@ -54,29 +54,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/idc/uinput-fpc.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-fpc.idc
 
-# NFC
-PRODUCT_PACKAGES += \
-    libnfc \
-    com.android.nfc_extras \
-    NfcNci \
-    nqnfcee_access.xml \
-    nqnfcse_access.xml \
-    SecureElement \
-    Tag \
-    com.android.nfc_extras \
-    android.hardware.nfc@1.1-service
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
-    $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
-
-# Permissions
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
-    frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
-    frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml \
-    frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml
-
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.device.rc
