0b42b92c9e8ad6316b4de069bb06f3bd3c280b7e
Jarl-Penguin <jarlpenguin@outlook.com>
2020-10-02 23:31:08 +0300

    cedric: Commonize HAL3 prop
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I695af5bc2fb613eda103274dccbddc1557039395

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -1,7 +1,6 @@
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.camera.gyro.disable=0 \
-    persist.camera.HAL3.enabled=1
+    persist.camera.gyro.disable=0
 
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
