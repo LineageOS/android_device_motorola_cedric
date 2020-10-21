841d4c6a89b45b6bcb0d6a4b77ca94500b44e82a
Jarl-Penguin <jarlpenguin@outlook.com>
2020-10-21 16:18:53 +0300

    cedric: rootdir: Cleanup makefile
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I563e6a05fa5ee98f5ff8d15fce38a17da0cc983d

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -2,9 +2,10 @@ LOCAL_PATH := $(call my-dir)
 
 # Init scripts
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.device.rc
-LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE := init.device.rc
+LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.device.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+LOCAL_SRC_FILES := etc/init.device.rc
+LOCAL_VENDOR_MODULE := true
+LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)
