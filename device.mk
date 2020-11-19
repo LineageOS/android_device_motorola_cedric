481c6b069533aeafdbe0a854854d668d8543a4df
nift4 <nift4@protonmail.com>
2020-11-19 16:06:06 +0100

    cedric: Remove unneeded sensor config and package
    * Remove unused sensors.rp (our sensor HAL handles the rear proximity sensor)
    * Remove unused qcom sensor config
    
    Change-Id: If3faecba76c1ee009c1daefdc6a0cf69b8f16aeb

--- device.mk
+++ device.mk
@@ -64,11 +64,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    motosh \
-    sensors.rp
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
+    motosh
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
